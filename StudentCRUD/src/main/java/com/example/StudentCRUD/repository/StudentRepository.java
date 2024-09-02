b87822e91fa47f9998f107c6e9250505f4e8df4e
abdoulfataho <yamyaogofataho@gmail.com>
2024-09-02 10:21:12 -0400

    finished

---
+++ StudentCRUD/src/main/java/com/example/StudentCRUD/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.example.StudentCRUD.repository;
+import com.example.StudentCRUD.model.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface  StudentRepository extends CrudRepository<Student, Long> {
+    // following are custom methods
+    Student findByAccountNo (Long accountNo) ;
+    List<Student> findByNameContaining(String name);
+}
+
